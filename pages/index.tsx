6ebb4705d862deea3180bfd678a3619cc85e4abb
unknown <yenloned@gmail.com>
2023-02-06 19:02:04 +0800

    setup strapi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from 'next'
+import axios from 'axios';
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: any) => {
+  //console.log(props.company_data)
+
+  let {description} = props.company_data.data.attributes
+
   return (
     <div>
-
+      {description}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  let company = await axios.get("http://localhost:1337/api/companies/1")
+  let company_data = company.data
+
+  if(!company_data) return {props: {}}
+
+  return {
+    props: {
+      company_data
+    }
+  }
 }
 
 export default Home
