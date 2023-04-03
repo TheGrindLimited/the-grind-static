44a877c95bc4b7442d1a4d85724255d2695ff2b1
unknown <yenloned@gmail.com>
2023-04-04 02:09:39 +0800

    feat: coach fix / player page

--- pages/players.tsx
+++ pages/players.tsx
@@ -12,15 +12,15 @@ const Coach = (props: any) => {
     return (
         <div>
             <Head>
-                <title>For Coach / School | The Grind</title>
+                <title>For Players / Young Athletes / Parents | The Grind</title>
             </Head>
             <div className="bg-[url(/assets/images/inner-page-hero-bg.png)] bg-cover bg-bottom bg-no-repeat pt-[82px] lg:pt-[106px]">
                 <div className="relative">
                     <div className="container">
                         <div className="relative w-full py-14 md:my-14 md:inline-block md:py-0 lg:my-[128px]">
                             <div className="heading relative mb-8 text-center lg:mb-0 lg:w-1/2 ltr:lg:text-left rtl:lg:text-right">
-                                <h6>For Coach / School</h6>
-                                <h4 className="!text-white">wanna elevate your training experience?</h4>
+                                <h6>For Players / Young Athletes / Parents</h6>
+                                <h4 className="!text-white">wanna get better at your sports?</h4>
                             </div>
                         </div>
                     </div>
@@ -28,52 +28,50 @@ const Coach = (props: any) => {
             </div>
 
             <div className='flex py-[41px] lg:py-[53px] justify-center gap-[15vw]'>
-                <div className='align-middle'>
-                    <div className="heading text-left">
-                        <h2>Schedule Training</h2>
-                        <h2>Plan for drills</h2>
-                        <h1 className='max-w-[430px]'>We not only help you to plan the drills for in person training, but you can set programs for each individual trainees to work on in other days.</h1>
-                    </div>
-                </div>
-                <div className='w-[240px] h-[480px]'>
-                    <img src={imageData.data[3].attributes.url} className="w-[240px] h-[480px]"/>
-                </div>
-            </div>
-
-            <div className='flex py-[41px] lg:py-[53px] justify-center gap-[15vw]'>
-                <div className='w-[240px] h-[480px]'>
-                    <img src={imageData.data[1].attributes.url} className="w-[240px] h-[480px]"/>
-                </div>
-                <div className='align-middle'>
-                    <div className="heading">
-                        <h2 className='max-w-[420px]'>Manage Team Activities</h2>
-                        <h1 className='max-w-[400px] text-left'>We make sure everyone in the group knows when and where the training is happening, and you know who is coming and who is not.</h1>
-                    </div>
-                </div>
-            </div>
-
-            <div className='flex py-[41px] lg:py-[53px] justify-center gap-[15vw]'>
-                <div className='align-middle'>
-                    <div className="heading text-left">
-                        <h2>Share knowledge</h2>
-                        <h2>Build community</h2>
-                        <h1 className='max-w-[400px]'>We help you build your sports training knowledge base, so that your trainees can look back anytime.</h1>
-                    </div>
-                </div>
                 <div className='w-[240px] h-[480px]'>
                     <img src={imageData.data[4].attributes.url} className="w-[240px] h-[480px]"/>
                 </div>
+                <div className='align-middle'>
+                    <div className="heading text-left">
+                        <h2 className='max-w-[430px]'>Have your own drill database</h2>
+                        <h1 className='max-w-[430px]'>We help you build your sports training knowledge base, so that you can look back anytime without asking your coach.</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex py-[41px] lg:py-[53px] justify-center gap-[15vw]'>
+                <div className='align-middle'>
+                    <div className="heading">
+                        <h2 className='max-w-[450px]'>Communicate with other players</h2>
+                        <h1 className='max-w-[400px] text-left'>You can discuss with your teammates & coaches on specific drills or concepts.</h1>
+                    </div>
+                </div>
+                <div className='w-[240px] h-[480px]'>
+                    <img src={imageData.data[5].attributes.url} className="w-[240px] h-[480px]"/>
+                </div>
             </div>
 
             <div className='flex py-[41px] lg:py-[53px] justify-center gap-[15vw]'>
                 <div className='w-[240px] h-[480px]'>
-                    <img src={imageData.data[6].attributes.url} className="w-[240px] h-[480px]"/>
+                    <img src={imageData.data[0].attributes.url} className="w-[240px] h-[480px]"/>
                 </div>
                 <div className='align-middle'>
                     <div className="heading text-left">
-                        <h2 className='max-w-[430px]'>Branding for coaches</h2>
-                        <h1 className='max-w-[400px]'>We build you a profile to represent your coaching style based on daily training highlight with your students.</h1>
+                        <h2 className='max-w-[450px]'>Show off your achievements & sports journey</h2>
+                        <h1 className='max-w-[400px]'>You can build your own sports career profile, share your progress & show off your personal highlights.</h1>
                     </div>
+                </div>
+            </div>
+
+            <div className='flex py-[41px] lg:py-[53px] justify-center gap-[15vw]'>
+                <div className='align-middle'>
+                    <div className="heading text-left">
+                        <h2 className='max-w-[430px]'>Explore new sports knowledge</h2>
+                        <h1 className='max-w-[400px]'>You can get to know other coaches, explore new sports knowledge and reach out to them.</h1>
+                    </div>
+                </div>
+                <div className='w-[240px] h-[480px]'>
+                    <img src={imageData.data[2].attributes.url} className="w-[240px] h-[480px]"/>
                 </div>
             </div>
         </div>
